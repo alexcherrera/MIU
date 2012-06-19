913117c0550a95030a08630e9e2de7303ba5f81a
alexcherrera <alexcherrera@me.com>
2012-06-18 20:51:29 -0400

    Fixing the form and adding other requirements

---
+++ Week4/Gold/js/json.js
@@ -0,0 +1,72 @@
+var jsonObject = {
+	"Schedule1": {
+		"major": ["Major Choice:", "Architecture"],
+		"cName": ["Course Name:", "Building Design"],
+		"cSection": ["Course Section:", "ARC134"],
+		"topicAndSec": ["Topic and Section:", "Building FrameWork, Section 1.2"],
+		"todaysDate": ["Today's Date:", "05/24/2012"],
+		"dueDate":["Due Date:", "05/26/2012"],
+		"option": ["Turn In Option:", "Email"],
+		"note": ["Note Section:", "This is a review for my test."]
+	},
+	"Schedule2": {
+		"major": ["Major Choice:", "Biology"],
+		"cName": ["Course Name:", "Introduction to Biology"],
+		"cSection": ["Course Section:", "BIO101"],
+		"topicAndSec": ["Topic and Section:", "Organic Compounds, Section 2.3"],
+		"todaysDate": ["Today's Date:", "05/24/2012"],
+		"dueDate":["Due Date:", "05/26/2012"],
+		"option": ["Turn In Option:", "Person"],
+		"note": ["Note Section:", "Remember to highlight important facts."]
+	},
+	"Schedule3": {
+		"major": ["Major Choice:", "Mathematics"],
+		"cName": ["Course Name:", "Calculus 1"],
+		"cSection": ["Course Section:", "MTH200"],
+		"topicAndSec": ["Topic and Section:", "Sine and Cosine, Section 3.1"],
+		"todaysDate": ["Today's Date:", "05/24/2012"],
+		"dueDate":["Due Date:", "05/26/2012"],
+		"option": ["Turn In Option:", "Email"],
+		"note": ["Note Section:", "First week and first learning Sine and Cosine."]
+	},
+	"Schedule4": {
+		"major": ["Major Choice:", "Accounting"],
+		"cName": ["Course Name:", "Tax Rates"],
+		"cSection": ["Course Section:", "ACC245"],
+		"topicAndSec": ["Topic and Section:", "Inflation, Section 4.4"],
+		"todaysDate": ["Today's Date:", "05/24/2012"],
+		"dueDate":["Due Date:", "05/26/2012"],
+		"option": ["Turn In Option:", "Person"],
+		"note": ["Note Section:", "Use a calculator for the math problems."]
+	},
+	"Schedule5": {
+		"major": ["Major Choice:", "Economics"],
+		"cName": ["Course Name:", "Intermediate Micro Economics"],
+		"cSection": ["Course Section:", "ECO312"],
+		"topicAndSec": ["Topic and Section:", "Monopoly Pricing, Section 5.1"],
+		"todaysDate": ["Today's Date:", "05/24/2012"],
+		"dueDate":["Due Date:", "05/26/2012"],
+		"option": ["Turn In Option:", "Email"],
+		"note": ["Note Section:", "Use notes from last class for this section."]
+	},
+	"Schedule6": {
+		"major": ["Major Choice:", "Computer Engineering"],
+		"cName": ["Course Name:", "Programming 1"],
+		"cSection": ["Course Section:", "CPE111"],
+		"topicAndSec": ["Topic and Section:", "Object Oriented Programming, Section 6.3"],
+		"todaysDate": ["Today's Date:", "05/24/2012"],
+		"dueDate":["Due Date:", "05/26/2012"],
+		"option": ["Turn In Option:", "Person"],
+		"note": ["Note Section:", "Use notes from the teachers power point."]
+	},
+	"Schedule7": {
+		"major": ["Major Choice:", "Electrical Engineering"],
+		"cName": ["Course Name:", "Circuit Theory"],
+		"cSection": ["Course Section:", "EEE323"],
+		"topicAndSec": ["Topic and Section:", "Electrical Current, Section 7.1"],
+		"todaysDate": ["Today's Date:", "05/24/2012"],
+		"dueDate":["Due Date:", "05/26/2012"],
+		"option": ["Turn In Option:", "Email"],
+		"note": ["Note Section:", "Read chapter and take notes."]
+	}
+}
---
+++ Week4/Gold/js/main.js
@@ -0,0 +1,44 @@
+/*
+Alexander Herrera
+Project 2
+For the browser I used Google Chrome, Safari web inspector hardly worked
+*/
+var parseCInfoForm = function(data) {
+    console.log(data);
+    for(var i; data.length; i++) {
+		var store = $.Storage.set(data.name[i],data.value[i]);
+		console.log(store);
+	
+	};
+
+};
+
+
+$(document).ready(function(){
+    var collForm = $('#collegeForm'),
+    	schedErrorsLink = $('#schedErrorsLink')
+	 ;
+        collForm.validate({
+        	invalidHandler: function(form, validator){
+        		schedErrorsLink.click();
+        		var html = '';
+        		for (var key in validator.submitted) {
+        			var label = $('label[for^="'+ key +'"]').not('[generated]');
+        			var legend = label.closest('fieldset').find('.ui-controlgroup-label');
+        			var fieldName = legend.length ? legend.text() : label.text();
+        			html += '<li>'+ fieldName +'</li>';
+
+        		};
+        		$("#scheduleErrors ul").html(html);
+        		
+        	},
+        	submitHandler: function(){
+        		var data = collForm.serializeArray();
+        		parseCInfoForm(data);
+        		
+        	}
+
+        });
+
+
+});
