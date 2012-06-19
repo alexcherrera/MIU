ac8f44f50794985c81a57db31c74033bc64304e6
alexcherrera <alexcherrera@me.com>
2012-06-19 15:32:08 -0400

    Added dateboxes

--- Week4/Gold/js/main.js
+++ Week4/Gold/js/main.js
@@ -15,26 +15,29 @@ var parseCInfoForm = function(data) {
 
 
 $(document).ready(function(){
-    var collForm = $('#collegeForm'),
-    	schedErrorsLink = $('#schedErrorsLink')
-	 ;
+    //var todaysDate = $('#todaysDate');
+    var collForm = $('#collegeForm');
+    var schedErrorsLink = $('#schedErrorsLink');
+      //  todaysDate.trigger('datebox');
         collForm.validate({
-        	invalidHandler: function(form, validator){
-        		schedErrorsLink.click();
-        		var html = '';
-        		for (var key in validator.submitted) {
-        			var label = $('label[for^="'+ key +'"]').not('[generated]');
-        			var legend = label.closest('fieldset').find('.ui-controlgroup-label');
-        			var fieldName = legend.length ? legend.text() : label.text();
-        			html += '<li>'+ fieldName +'</li>';
+        invalidHandler: function(form, validator){
+                schedErrorsLink.click();
+        	var html = '';
+        	for (var key in validator.submitted) {
+        	       var label = $('label[for^="'+ key +'"]').not('[generated]');
+        		var legend = label.closest('fieldset').find('.ui-controlgroup-label');
+        		var fieldName = legend.length ? legend.text() : label.text();
+        		html += '<li>'+ fieldName +'</li>';
 
-        		};
-        		$("#scheduleErrors ul").html(html);
-        		
+        	};
+        	$("#scheduleErrors ul").html(html);	
         	},
         	submitHandler: function(){
-        		var data = collForm.serializeArray();
-        		parseCInfoForm(data);
+        		$('courseInfoForm').submit(function() {
+                        console.log($(this).serializeArray());
+                        return false;
+                });
+        		
         		
         	}
 
